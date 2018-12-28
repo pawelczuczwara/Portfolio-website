d7c41560ac12fdc9957fb6fdb37723ebfc004a8d
pawelczuczwara <pawel.czuczwara@gmail.com>
2018-12-28 11:25:21 +0100

    V1.5 github content added

---
+++ scr/content.js
@@ -0,0 +1,112 @@
+'use strict'
+
+const menuData = [
+    {
+        name: 'coding',
+        url: 'https://github.com/pawelczuczwara',
+    },
+    {
+        name: 'linkedin',
+        url: 'https://www.linkedin.com/in/czuczwara/',
+    },
+    // {
+    //     name: 'digital maps',
+    //     url: '#',
+    // },
+    {
+        name: 'about',
+        url: '#about',
+    },
+];
+
+const bigProjects = [
+    {
+        p_des: 'Github resources',
+        url: 'https://github.com/pawelczuczwara',
+        url_txt: '',
+        img_src: 'p_idea_650.jpg',
+        img_alt: 'github resources',
+        img_srcset: 'img/p_idea_325.jpg 325w, img/p_idea_650.jpg 650w, img/p_idea_1024.jpg 1024w',
+        git: 'https://github.com/pawelczuczwara',
+        git_txt: '//github.com/pawelczuczwara'
+    }
+];
+
+const allProjects = [
+    {
+        p_des: 'JavaScript arcade game',
+        url: 'https://pawelc.eu/program/rescue/',
+        url_txt: '//pawelc.eu/rescue/',
+        img_src: 'p_shark_450.jpg',
+        img_alt: 'JavaScript Arcade Game',
+        git: 'https://github.com/pawelczuczwara/arcade-game',
+        git_txt: '//github.com/arcade-game/'
+    },
+    {
+        p_des: 'Memory game',
+        url: 'https://pawelc.eu/program/memory/',
+        url_txt: '//pawelc.eu/memory/',
+        img_src: 'p_memory_450.jpg',
+        img_alt: 'memory game',
+        git: 'https://github.com/pawelczuczwara/MemoryGame',
+        git_txt: '//github.com/memory/'
+    },
+    {
+        p_des: 'Jasmine framework',
+        url: 'https://pawelc.eu/program/jasmine/',
+        url_txt: '//pawelc.eu/jasmine/',
+        img_src: 'p_test_450.jpg',
+        img_alt: 'Jasmine Framework RSS Reader',
+        git: 'https://github.com/pawelczuczwara/RSS-FeedReader',
+        git_txt: '//github.com/RSS-FeedReader/'
+    },
+    {
+        p_des: 'Mobile application design',
+        url: 'https://www.behance.net/gallery/29794267/Safe-Gluten-Free-Travel/',
+        url_txt: '//behance.net/',
+        img_src: 'p_mobile_450.jpg',
+        img_alt: 'mobile',
+        git: '',
+        git_txt: ''
+    },
+    {
+        p_des: 'Drone services website',
+        url: 'https://pablos.eu/wp/',
+        url_txt: '//pablos.eu/wp/',
+        img_src: 'p_dron_450.jpg',
+        img_alt: 'drone',
+        git: '',
+        git_txt: ''
+    },
+
+    {
+        p_des: 'Commercial photography',
+        url: 'https://www.shutterstock.com/g/Pawel+Czuczwara/',
+        url_txt: '//shutterstock.com/',
+        img_src: 'p_photo_450.jpg',
+        img_alt: 'photography',
+        git: '',
+        git_txt: ''
+    },
+    {
+        p_des: 'E-commerce shop',
+        url: 'https://pablos.eu/st/',
+        url_txt: '//pablos.eu/st/',
+        img_src: 'p_shop_450.jpg',
+        img_alt: 'e-commerce shop',
+        git: '',
+        git_txt: ''
+    },
+
+    // {
+    //     p_des: '',
+    //     url: '',
+    //     url_txt: '',
+    //     img_src: '',
+    //     img_alt: '',
+    //     git: '',
+    //     git_txt: ''
+    // },
+];
+
+
--- scr/script.js
+++ scr/script.js
@@ -3,79 +3,7 @@
 const navButton = document.querySelector('.header__button');
 const navMenu = document.querySelector('.nav');
 const viewSize = document.querySelector('.view__size');
-let viewX;
-let viewY;
-let sizing;
-let viewW;
 
-const bigProjects = [
-    {
-        p_des: 'Github resources',
-        url: 'https://github.com/pawelczuczwara',
-        url_txt: '',
-        img_src: 'p_idea_650.jpg',
-        img_alt: 'github resources',
-        img_srcset: 'img/p_idea_325.jpg 325w, img/p_idea_650.jpg 650w, img/p_idea_1024.jpg 1024w',
-        git: 'https://github.com/pawelczuczwara',
-        git_txt: '//github.com/pawelczuczwara'
-    } ];
-
-const allProjects = [
-    {
-        p_des: 'Drone services website',
-        url: 'https://pablos.eu/wp/',
-        url_txt: '//pablos.eu/wp/',
-        img_src: 'p_dron_450.jpg',
-        img_alt: 'drone',
-        git: '',
-        git_txt: ''
-    },
-    {
-        p_des: 'Mobile application design',
-        url: 'https://www.behance.net/gallery/29794267/Safe-Gluten-Free-Travel/',
-        url_txt: '//behance.net/',
-        img_src: 'p_mobile_450.jpg',
-        img_alt: 'mobile',
-        git: '',
-        git_txt: ''
-    },
-    {
-        p_des: 'Memory Game',
-        url: 'https://pawelc.eu/program/memory/',
-        url_txt: '//pawelc.eu/memory/',
-        img_src: 'p_memory_450.jpg',
-        img_alt: 'memory game',
-        git: 'https://github.com/pawelczuczwara/MemoryGame',
-        git_txt: '//github.com/memory/'
-    },
-    {
-        p_des: 'Commercial photography',
-        url: 'https://www.shutterstock.com/g/Pawel+Czuczwara/',
-        url_txt: '//shutterstock.com/',
-        img_src: 'p_photo_450.jpg',
-        img_alt: 'photography',
-        git: '',
-        git_txt: ''
-    },
-    {
-        p_des: 'E-commerce shop',
-        url: 'https://pablos.eu/st/',
-        url_txt: '//pablos.eu/st/',
-        img_src: 'p_shop_450.jpg',
-        img_alt: 'e-commerce shop',
-        git: '',
-        git_txt: ''
-    }
-    // {
-    //     p_des: '',
-    //     url: '',
-    //     url_txt: '',
-    //     img_src: '',
-    //     img_alt: '',
-    //     git: '',
-    //     git_txt: ''
-    // },
-];
 class CreateContent
 {
     constructor(data, template, entry) {
@@ -85,12 +13,12 @@ class CreateContent
     }
 
     _getTemplate() {
-        let tepmlate_HTML = document.querySelector(this.template_class).innerHTML;
-        return Handlebars.compile(tepmlate_HTML);
+        let html = document.querySelector(this.template_class).innerHTML;
+        return Handlebars.compile(html);
     }
 
-    _insertHTML(compiled_html) {
-        this.entry_node.insertAdjacentHTML('beforeend', compiled_html);
+    _insertHTML(html) {
+        this.entry_node.insertAdjacentHTML('beforeend', html);
     }
 
     createHTML() {
@@ -106,12 +34,16 @@ class CreateContent
 
 }
 
+const menu    = new CreateContent(menuData,    '.menu_template',      '.nav__list');
 const big_prj = new CreateContent(bigProjects, '.big_entry_template', '.section');
 const sma_prj = new CreateContent(allProjects, '.entry_template',     '.section');
-
-
 // Provides information about window size
 function startSize() {
+    let viewX,
+        viewY,
+        sizing,
+        viewW;
+
     viewY = window.innerHeight;
     viewX = window.innerWidth;
 
@@ -133,8 +65,10 @@ navButton.addEventListener('click', () => {
 window.addEventListener('resize', () => startSize());
 
 (function init() {
+    menu   .createHTML();
     big_prj.createHTML();
     sma_prj.createHTML();
-    startSize();
+
+            startSize();
 })();
 
